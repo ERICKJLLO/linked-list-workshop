4c7bddb2b8a6e956ad69e08dcfb28264ac744a57
erickjllo <erickjllo07@gmail.com>
2025-03-03 17:33:40 -0500

    inicial

--- datastruct/exercises/solution_1.py
+++ datastruct/exercises/solution_1.py
@@ -1,6 +1,4 @@
 from datastruct.classes.lists import LinkedList
 
-
 def add_two_numbers(l1: LinkedList[int], l2: LinkedList[int]) -> LinkedList[int]:
-    # Implement solution here
     pass
--- datastruct/exercises/solution_2.py
+++ datastruct/exercises/solution_2.py
@@ -1,6 +1,4 @@
 from datastruct.classes.lists import LinkedList
 
-
 def swap_nodes_in_pairs(linked_list: LinkedList[int]) -> LinkedList[int]:
-    # Implement solution here
     pass
